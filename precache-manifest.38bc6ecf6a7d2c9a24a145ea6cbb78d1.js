2540c9f6d6acdbf23bdeb592b04e390817158f3c
elcajo <46391075+elcajo@users.noreply.github.com>
2020-10-21 13:03:30 +0800

    Updates

---
+++ precache-manifest.38bc6ecf6a7d2c9a24a145ea6cbb78d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bc417d5cc3be0b036cfe7a47020730",
+    "url": "/focalator/index.html"
+  },
+  {
+    "revision": "0de23ee3d1fd0b6dd093",
+    "url": "/focalator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b2dffc7452bea5721af5",
+    "url": "/focalator/static/css/main.b8adf444.chunk.css"
+  },
+  {
+    "revision": "0de23ee3d1fd0b6dd093",
+    "url": "/focalator/static/js/2.a138cca2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/focalator/static/js/2.a138cca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dffc7452bea5721af5",
+    "url": "/focalator/static/js/main.8ac07e06.chunk.js"
+  },
+  {
+    "revision": "5c0fc6dd8f3253b1f165",
+    "url": "/focalator/static/js/runtime-main.bc000308.js"
+  }
+]);
